ad7fd35010a31e9f5a5452e16722ef751fd61655
Abdullahk70 <scientificgenius701924@gmail.com>
2023-10-12 18:13:08 +0500

    added further json implementation

--- src/components/Homescreen.js
+++ src/components/Homescreen.js
@@ -4,24 +4,21 @@ import Sportsshoes from "./Sportsshoes"
 import Casualshoes from "./Casualshoes"
 import Formalshoes from "./Formalshoes"
 import Footer from "./Footer"
-import lg1984 from "./img/1984.png"
+import Motto from "./motto"
+
 
 
 const Homescreen=()=>{
     return  <><Navbar/>
     <Carousel/>
-    <div className=" bg-success row container-fluid" style={{padding:"1em"}}>
-     <div className="col align-self-center text-danger text-lg-center fw-bold"> <p style={{font:"20px"}}>WE REDEFINE</p></div>
-     <div className="col align-self-center rounded"><img className="rounded-circle" src={lg1984} alt="" /></div>
-     <div className="col align-self-center text-danger text-lg-center fw-bold"> <p>COMFORT</p></div>
-
-    </div>
+    
     <h2 className="m-4 ">Men Sports Wear</h2>
     <Sportsshoes/>
     <h2 className="m-4">Men Casual Wear</h2>
     <Casualshoes/>
     <h2 className="m-4">Men Formal Wear</h2>
     <Formalshoes/>
+    <Motto/>
     <Footer/> 
     </>
     
---
+++ src/components/motto.js
@@ -0,0 +1,10 @@
+import lg1984 from "./img/1984.png"
+const Motto=()=>{
+    return <div className=" opacity-100 row container-fluid mt-3 rounded-5" style={{padding:"1em",background:"#cfd1d0"}}>
+     <div className="col align-self-center text-danger text-lg-center fw-bold fs-5"> <p>WE REDEFINE</p></div>
+     <div className="col align-self-center rounded ms-5"><img className="rounded-circle" src={lg1984} alt="" /></div>
+     <div className="col align-self-center text-danger text-lg-center fw-bold fs-5"> <p>COMFORT</p></div>
+
+    </div>
+}
+export default Motto;
