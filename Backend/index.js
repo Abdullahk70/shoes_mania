a8653af29ad550e34c0deb08508e6e21bdb7fdb7
Abdullahk70 <scientificgenius701924@gmail.com>
2023-11-17 14:08:41 +0500

    mongoDB connected using mongoose

--- Backend/index.js
+++ Backend/index.js
@@ -1,18 +1,27 @@
 import http from "http"
 import nodemon from "nodemon";
-const server=http.createServer((req,res)=>{
-  if(req.url==="/"){
-    res.write("Loading home page");
-    res.end();
-  }
-  if(req.url==="/Products"){
-    res.write("Loading Products page");
-    res.end();
-  }
-  if(req.url==="/Services"){
-    res.write("Loading Services page");
-    res.end();
-  }
-})
+import mongoose from "mongoose";
 
-server.listen(8000)
+const url="mongodb+srv://abdullah:123@cluster0.qfbdxft.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(url).then(()=>{console.log("databaseConnected")}).catch(()=>{console.log("database not Connected")});
+
+
+
+
+
+// const server=http.createServer((req,res)=>{
+//   if(req.url==="/"){
+//     res.write("Loading home page");
+//     res.end();
+//   }
+//   if(req.url==="/Products"){
+//     res.write("Loading Products page");
+//     res.end();
+//   }
+//   if(req.url==="/Services"){
+//     res.write("Loading Services page");
+//     res.end();
+//   }
+// })
+
+// server.listen(8000)
