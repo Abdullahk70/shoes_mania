50839076b277136edc7a6981d9b2bcfce60214f0
Abdullahk70 <scientificgenius701924@gmail.com>
2023-11-28 22:56:52 +0500

    data is now successfully getting sent to mongoDB

--- Backend/controllers/shoe.js
+++ Backend/controllers/shoe.js
@@ -1,7 +1,21 @@
+import { shoeModel } from "../models/shoes.js";
+
 export const shoeGetController = (req, res) => {
     res.write("Shoe page loading. ");
     res.end();
 }
 export const shoePostController = (req, res) => {
     console.log(req.body);
+    const model=new shoeModel({
+      name:req.body.name,
+      id:req.body.id,
+      pic:req.body.pic,
+      price:req.body.price,
+      description:req.body.description,
+      category:req.body.description,
+      uploadDate:new Date(),
+      size:req.body.size,
+    });
+    model.save();
+
 }
