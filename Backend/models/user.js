763b816089fc89cbf7bee29b38474074dfaa1556
Abdullahk70 <scientificgenius701924@gmail.com>
2023-12-05 22:39:56 +0500

    added user jsonModel

---
+++ Backend/models/user.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+   email: String,
+   password: String,
+   phoneNumber: String,
+    
+});
+
+export const userModel = mongoose.model("user", userSchema);
