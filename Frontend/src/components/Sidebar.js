ed9a3d03db0607ec4c5061ad52bfbf3ccf03e433
Abdullahk70 <scientificgenius701924@gmail.com>
2023-12-14 19:23:58 +0500

    made further changes and added icons in the sidepanel

--- Frontend/src/components/Sidebar.js
+++ Frontend/src/components/Sidebar.js
@@ -1,13 +1,53 @@
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus, faEdit, faTrash, faEye, faChartBar, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 const Sidebar = () => {
   return (
-    <div class=" mt-5 bg-secondary" style={{ width: "18rem",backgroundColor:"#e4e7eb"}}>
-      <ul class="" style={{backgroundColor:"#e4e7eb"}}> 
-        <li class="list-group-item " ><Link to="/adminpanel/create" style={{textDecoration:"none"}}><p style={{marginTop:"15px"}}>Create Record</p></Link></li>
-        <li class="list-group-item"><Link to="/adminpanel/update" style={{textDecoration:"none"}}><p style={{marginTop:"15px"}}>Update Record</p></Link></li>
-        <li class="list-group-item"><Link to="/adminpanel/delete" style={{textDecoration:"none"}}><p style={{marginTop:"15px"}}>Delete Record</p></Link></li>
-        <li class="list-group-item"><Link to="/adminpanel/view" style={{ textDecoration:"none"}}><p style={{marginTop:"15px"}}>View Record</p></Link></li>
+    <div className="mt-5 bg-secondary" style={{ width: "18rem", backgroundColor: "#e4e7eb" }}>
+      <ul className="" style={{ backgroundColor: "#e4e7eb" }}>
+        <li className="list-group-item d-flex">
+          <Link to="/adminpanel/create" style={{ textDecoration: "none" }}>
+            <div className="d-flex">
+            <FontAwesomeIcon icon={faPlus} style={{marginTop:"20px"}}/> <p className="font-weight-bold " style={{ marginTop: "15px",marginLeft:"8px" }}>Create Record</p>
+            </div>
+          </Link>
+        </li>
+        <li className="list-group-item">
+          <Link to="/adminpanel/update" style={{ textDecoration: "none" }}>
+            <div className="d-flex">
+            <FontAwesomeIcon icon={faEdit} style={{marginTop:"20px"}}/> <p style={{ marginTop: "15px",marginLeft:"5px" }}>Update Record</p>
+            </div>
+          </Link>
+        </li>
+        <li className="list-group-item">
+          <Link to="/adminpanel/delete" style={{ textDecoration: "none" }}>
+            <div className="d-flex">
+            <FontAwesomeIcon icon={faTrash} style={{marginTop:"20px"}}/> <p style={{ marginTop: "15px",marginLeft:"7px" }}>Delete Record</p>
+            </div>
+          </Link>
+        </li>
+        <li className="list-group-item">
+          <Link to="/adminpanel/view" style={{ textDecoration: "none" }}>
+            <div className="d-flex">
+            <FontAwesomeIcon icon={faEye} style={{marginTop:"20px"}} /> <p style={{ marginTop: "15px",marginLeft:"5px" }}>View Record</p>
+            </div>
+          </Link>
+        </li>
+        <li className="list-group-item">
+          <Link to="/adminpanel/delete" style={{ textDecoration: "none" }}>
+            <div className="d-flex">
+            <FontAwesomeIcon icon={faChartBar} style={{marginTop:"20px"}}  /> <p style={{ marginTop: "15px",marginLeft:"7px" }}>Analytics</p>
+            </div>
+          </Link>
+        </li>
+        <li className="list-group-item">
+          <Link to="/adminpanel/view" style={{ textDecoration: "none" }}>
+            <div className="d-flex">
+            <FontAwesomeIcon icon={faEnvelope} style={{marginTop:"20px"}} /> <p style={{ marginTop: "15px",marginLeft:"7px" }}>Messages</p>
+            </div>
+          </Link>
+        </li>
       </ul>
     </div>
   );
